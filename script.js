0cbd98b747bcb009f5d9bf6d480933a1e7aa0ace
Gepoverlow <gepeoverload@gmail.com>
2021-08-21 16:35:42 +0200

    testing today

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+//Factory Function INTRO ////////////////////////////////////////////////
+
+const bookFactory = (title, author, pages) => {
+  const returnInfo = () =>
+    console.log(`${title} written by ${author} has ${pages} pages`);
+  return { title, author, pages, returnInfo };
+};
+
+const Book = bookFactory("Harry Potter", "JK Rowling", 136);
+
+console.log(Book.title);
+console.log(Book.author);
+console.log(Book.pages);
+Book.returnInfo();
+
+///////////////////////////////////////////////////////////////////////////
