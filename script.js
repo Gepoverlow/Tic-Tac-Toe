fdeda434be78033c6524ffa39d1067ee8368b8fd
Gepoverlow <gepeoverload@gmail.com>
2021-08-23 17:38:50 +0200

    render almost done

--- script.js
+++ script.js
@@ -79,18 +79,77 @@
 
 //MODULES
 
+/* INTENTO 1
+
+const createPlayer = function (input, number) {
+  let playerName = input;
+  let playerNumber = number;
+  return { playerName, playerNumber };
+};
+
 const gameBoard = (() => {
-  let gameboard = [1, 2, 3];
-  return { gameboard };
+  let gameboard = [];
+
+  const playButton = document.querySelector(".btn-play");
+
+  playButton.addEventListener("click", function () {
+    const inputOne = document.getElementById("input-one").value;
+    const inputTwo = document.getElementById("input-two").value;
+
+    let playerOne = createPlayer(`${inputOne}`, 1); //input
+    let playerTwo = createPlayer(`${inputTwo}`, 2); //input
+
+    console.log(playerOne, playerTwo);
+    return { playerOne, playerTwo };
+  });
 })();
 
-console.log(gameBoard);
-console.log(gameBoard.gameboard);
+INTENTO 1 */
 
-const displayController = () => {};
-
-const playerFactory = (name, playerNumber) => {
-  const givePlayerInfo = () =>
-    console.log(`${name} is player number ${playerNumber}`);
-  return { name, playerNumber, givePlayerInfo };
+const createPlayer = function (input, score) {
+  let playerName = input;
+  let playerScore = score;
+  return { playerName, playerScore };
 };
+
+const gameBoard = (() => {
+  let gameBoardArray = ["o", "x", "o", "x", "x", "x", "o", "o", "x"];
+
+  function createBlock() {
+    let parent = document.querySelector(".container-game");
+    let block = document.createElement("div");
+    block.classList.add("block");
+    parent.appendChild(block);
+  }
+
+  gameBoardArray.forEach(createBlock);
+
+  function renderValues() {
+    let allBlocks = document.querySelectorAll(".block");
+    for (let i = 0; i < gameBoardArray.length; i++) {
+      allBlocks[i].textContent = gameBoardArray[i];
+    }
+  }
+
+  return { gameBoardArray, renderValues };
+})();
+
+gameBoard.renderValues();
+console.log(gameBoard.gameBoardArray);
+
+// const displayController = (() => {
+//   const playButton = document.getElementById("btn-play");
+
+//   playButton.addEventListener("click", function () {
+//     const playerOneInput = document.getElementById("input-one").value;
+//     const playerTwoInput = document.getElementById("input-two").value;
+
+//     document.querySelector(".container-playerSelect").style.display = "none";
+//     document.querySelector(".container-game").style.display = "grid";
+
+//     let playerOne = createPlayer(playerOneInput, 1);
+//     let playerTwo = createPlayer(playerTwoInput, 2);
+
+//     console.log(playerOne, playerTwo);
+//   });
+// })();
