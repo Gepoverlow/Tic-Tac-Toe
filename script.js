230db86ccc0c7cdce94e03fc51b2bb5f4b869e40
Gepoverlow <gepeoverload@gmail.com>
2021-08-24 18:01:06 +0200

    progress

--- script.js
+++ script.js
@@ -1,125 +1,37 @@
 "use strict";
-// //Factory Function INTRO ////////////////////////////////////////////////
 
-// const bookFactory = (title, author, pages) => {
-//   const returnInfo = () =>
-//     console.log(`${title} written by ${author} has ${pages} pages`);
-//   return { title, author, pages, returnInfo };
-// };
+const createPlayer = function (name, scoreType) {
+  let playerName = name;
+  let playerScore = scoreType;
+  const getName = () => playerName;
 
-// const Book = bookFactory("Harry Potter", "JK Rowling", 136);
-
-// console.log(Book);
-// console.log(Book.title);
-// console.log(Book.author);
-// console.log(Book.pages);
-// Book.returnInfo();
-
-// ///////////////////////////////////////////////////////////////////////////
-
-// const playerFactory = (name, playerNumber) => {
-//   const givePlayerInfo = () =>
-//     console.log(`${name} is player number ${playerNumber}`);
-//   return { name, playerNumber, givePlayerInfo };
-// };
-
-// const luke = playerFactory("Luke", 1);
-// const palo = playerFactory("Palo", 2);
-
-// console.log(luke);
-// console.log(palo);
-// luke.givePlayerInfo();
-// palo.givePlayerInfo();
-
-// // Modules INTRO ///////////////////////////////////////////////////////////
-
-//  const calculator = (() => {
-//    const add = (a, b) => a + b;
-//    const sub = (a, b) => a - b;
-//    const mul = (a, b) => a * b;
-//    const div = (a, b) => a / b;
-//    return {
-//     add,
-//     sub,
-//     mul,
-//     div,
-//   };
-// })();
-
-// calculator.add(3, 5); // 8
-// calculator.sub(6, 2); // 4
-// calculator.mul(14, 5534); // 77476
-
-// //////////////////////////////////////// REVEALING MODULE PATTERN ///////////////////////////
-
-// var myModule = (function () {
-//   "use strict";
-
-//   var _privateProperty = "Hello World";
-//   var publicProperty = "I am a public property";
-
-//   function _privateMethod() {
-//     console.log(_privateProperty);
-//   }
-
-//   function publicMethod() {
-//     _privateMethod();
-//   }
-
-//   return {
-//     publicMethod,
-//     publicProperty,
-//   };
-// })();
-
-// myModule.publicMethod(); // outputs 'Hello World'
-// console.log(myModule.publicProperty); // outputs 'I am a public property'
-// console.log(myModule._privateProperty); // is undefined protected by the module closure
-// myModule._privateMethod(); // is TypeError protected by the module closure
-
-//MODULES
-
-/* INTENTO 1
-
-const createPlayer = function (input, number) {
-  let playerName = input;
-  let playerNumber = number;
-  return { playerName, playerNumber };
+  return { playerName, playerScore, getName };
 };
 
 const gameBoard = (() => {
-  let gameboard = [];
+  //
+  let gameBoardArray = ["", "", "", "", "", "", "", "", ""];
+  //
+  let menuContainer = document.querySelector(".container-playerSelect");
+  let gameContainer = document.querySelector(".container-game");
+  let playButton = document.getElementById("btn-play");
+  //   let inputOne = document.getElementById("input-one");
+  //   let inputTwo = document.getElementById("input-two");
 
-  const playButton = document.querySelector(".btn-play");
-
+  //
   playButton.addEventListener("click", function () {
-    const inputOne = document.getElementById("input-one").value;
-    const inputTwo = document.getElementById("input-two").value;
-
-    let playerOne = createPlayer(`${inputOne}`, 1); //input
-    let playerTwo = createPlayer(`${inputTwo}`, 2); //input
-
-    console.log(playerOne, playerTwo);
-    return { playerOne, playerTwo };
+    menuContainer.style.display = "none";
+    gameContainer.style.display = "grid";
+    //
+    // let playerOne = createPlayer(`${inputOne.value}` || "Player One", "X");
+    // let playerTwo = createPlayer(`${inputTwo.value}` || "Player Two", "O");
+    //
   });
-})();
-
-INTENTO 1 */
-
-const createPlayer = function (input, score) {
-  let playerName = input;
-  let playerScore = score;
-  return { playerName, playerScore };
-};
-
-const gameBoard = (() => {
-  let gameBoardArray = ["o", "x", "o", "x", "x", "x", "o", "o", "x"];
 
   function createBlock() {
-    let parent = document.querySelector(".container-game");
     let block = document.createElement("div");
     block.classList.add("block");
-    parent.appendChild(block);
+    gameContainer.appendChild(block);
   }
 
   gameBoardArray.forEach(createBlock);
@@ -127,29 +39,31 @@ const gameBoard = (() => {
   function renderValues() {
     let allBlocks = document.querySelectorAll(".block");
     for (let i = 0; i < gameBoardArray.length; i++) {
-      allBlocks[i].textContent = gameBoardArray[i];
+      allBlocks[i].innerText = gameBoardArray[i];
     }
   }
 
-  return { gameBoardArray, renderValues };
+  return { gameBoardArray, renderValues, createBlock };
 })();
 
-gameBoard.renderValues();
-console.log(gameBoard.gameBoardArray);
-
-// const displayController = (() => {
-//   const playButton = document.getElementById("btn-play");
-
-//   playButton.addEventListener("click", function () {
-//     const playerOneInput = document.getElementById("input-one").value;
-//     const playerTwoInput = document.getElementById("input-two").value;
-
-//     document.querySelector(".container-playerSelect").style.display = "none";
-//     document.querySelector(".container-game").style.display = "grid";
-
-//     let playerOne = createPlayer(playerOneInput, 1);
-//     let playerTwo = createPlayer(playerTwoInput, 2);
-
-//     console.log(playerOne, playerTwo);
-//   });
-// })();
+const displayController = (() => {
+  //
+  let allBlocks = document.querySelectorAll(".block");
+  let game = false;
+  //
+  //
+  function playerMove() {
+    for (let i = 0; i < allBlocks.length; i++) {
+      let eachBlock = allBlocks[i];
+      eachBlock.addEventListener("click", function () {
+        if (eachBlock.innerHTML === "") {
+          gameBoard.gameBoardArray.splice(i, 1, game ? "O" : "X");
+          gameBoard.renderValues();
+          game = !game;
+          console.log(gameBoard.gameBoardArray);
+        }
+      });
+    }
+  }
+  playerMove();
+})();
