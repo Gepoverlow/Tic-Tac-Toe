03a1853dee0884ddb3df2095ffa1ad33402080eb
Gepoverlow <gepeoverload@gmail.com>
2021-08-25 15:37:19 +0200

    liking the look of this

--- script.js
+++ script.js
@@ -4,66 +4,98 @@ const createPlayer = function (name, scoreType) {
   let playerName = name;
   let playerScore = scoreType;
   const getName = () => playerName;
+  const getScore = () => playerScore;
 
-  return { playerName, playerScore, getName };
+  return { playerName, playerScore, getName, getScore };
 };
 
 const gameBoard = (() => {
   //
   let gameBoardArray = ["", "", "", "", "", "", "", "", ""];
   //
-  let menuContainer = document.querySelector(".container-playerSelect");
   let gameContainer = document.querySelector(".container-game");
-  let playButton = document.getElementById("btn-play");
-  //   let inputOne = document.getElementById("input-one");
-  //   let inputTwo = document.getElementById("input-two");
+  let tableContainer = document.querySelector(".table");
 
   //
-  playButton.addEventListener("click", function () {
-    menuContainer.style.display = "none";
-    gameContainer.style.display = "grid";
-    //
-    // let playerOne = createPlayer(`${inputOne.value}` || "Player One", "X");
-    // let playerTwo = createPlayer(`${inputTwo.value}` || "Player Two", "O");
-    //
-  });
-
   function createBlock() {
     let block = document.createElement("div");
     block.classList.add("block");
-    gameContainer.appendChild(block);
+    gameContainer.appendChild(tableContainer);
+    tableContainer.appendChild(block);
+  }
+
+  function renderValues(array) {
+    let allBlocks = document.querySelectorAll(".block");
+    for (let i = 0; i < gameBoardArray.length; i++) {
+      allBlocks[i].innerText = array[i];
+    }
   }
 
   gameBoardArray.forEach(createBlock);
 
-  function renderValues() {
-    let allBlocks = document.querySelectorAll(".block");
-    for (let i = 0; i < gameBoardArray.length; i++) {
-      allBlocks[i].innerText = gameBoardArray[i];
-    }
-  }
-
-  return { gameBoardArray, renderValues, createBlock };
+  return { gameBoardArray, renderValues };
 })();
 
 const displayController = (() => {
+  let game = false;
   //
   let allBlocks = document.querySelectorAll(".block");
-  let game = false;
+  let playButton = document.getElementById("btn-play");
+  let restartButton = document.getElementById("btn-restart");
+  let menuContainer = document.querySelector(".container-playerSelect");
+  let gameContainer = document.querySelector(".container-game");
+  //
+  let playerOne;
+  let playerTwo;
+  //
+  playButton.addEventListener("click", function () {
+    let inputOne = document.getElementById("input-one").value;
+    let inputTwo = document.getElementById("input-two").value;
+    let playerOneName = document.getElementById("player1");
+    let playerTwoName = document.getElementById("player2");
+    //
+    menuContainer.style.display = "none";
+    gameContainer.style.display = "flex";
+    //
+    playerOne = createPlayer(`${inputOne}`, "X");
+    playerTwo = createPlayer(`${inputTwo}`, "O");
+    //
+    playerOneName.textContent = `${playerOne.getName()}` || "Player One";
+    playerTwoName.textContent = `${playerTwo.getName()}` || "Player Two";
+    //
+  });
   //
   //
   function playerMove() {
     for (let i = 0; i < allBlocks.length; i++) {
       let eachBlock = allBlocks[i];
       eachBlock.addEventListener("click", function () {
-        if (eachBlock.innerHTML === "") {
+        if (gameBoard.gameBoardArray[i] === "") {
           gameBoard.gameBoardArray.splice(i, 1, game ? "O" : "X");
-          gameBoard.renderValues();
-          game = !game;
           console.log(gameBoard.gameBoardArray);
+          gameBoard.renderValues(gameBoard.gameBoardArray);
+          game = !game;
         }
       });
     }
   }
+
   playerMove();
+
+  restartButton.addEventListener("click", function () {
+    let allBlocksCleaned = document.querySelectorAll(".block");
+    menuContainer.style.display = "flex";
+    gameContainer.style.display = "none";
+    //
+    gameBoard.gameBoardArray = ["", "", "", "", "", "", "", "", ""];
+    //
+    for (let i = 0; i < allBlocksCleaned.length; i++) {
+      let eachBlockCleaned = allBlocksCleaned[i];
+      eachBlockCleaned.innerHTML = "";
+    }
+    game = false;
+  });
+  //
+
+  //
 })();
