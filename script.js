53e781a4490db57346e53c8d0eb811e57cb5d915
Gepoverlow <gepeoverload@gmail.com>
2021-08-26 15:20:43 +0200

    almost done!

--- script.js
+++ script.js
@@ -44,6 +44,8 @@ const displayController = (() => {
   let restartButton = document.getElementById("btn-restart");
   let menuContainer = document.querySelector(".container-playerSelect");
   let gameContainer = document.querySelector(".container-game");
+  let playerOneName = document.querySelector("#player1");
+  let playerTwoName = document.querySelector("#player2");
   //
   let playerOne;
   let playerTwo;
@@ -54,16 +56,28 @@ const displayController = (() => {
       eachBlock.addEventListener("click", function () {
         if (gameBoard.gameBoardArray[i] === "") {
           gameBoard.gameBoardArray.splice(i, 1, game ? "O" : "X");
-          console.log(gameBoard.gameBoardArray);
+          //
+          eachTurn();
+          checkWinner(gameBoard.gameBoardArray);
+          //
           gameBoard.renderValues(gameBoard.gameBoardArray);
           game = !game;
-          checkWinner(gameBoard.gameBoardArray);
         }
       });
     }
   }
-  //
   playerMove();
+  //
+  function eachTurn() {
+    if (game) {
+      playerOneName.classList.add("currentTurn");
+      playerTwoName.classList.remove("currentTurn");
+    } else if (!game) {
+      playerOneName.classList.remove("currentTurn");
+      playerTwoName.classList.add("currentTurn");
+    }
+  }
+
   //
   function checkWinner(arr) {
     if (arr[0] === "X" && arr[1] === "X" && arr[2] === "X") {
@@ -101,6 +115,8 @@ const displayController = (() => {
     }
   }
   //
+  function gameOver() {}
+  //
   playButton.addEventListener("click", function () {
     let inputOne = document.getElementById("input-one").value;
     let inputTwo = document.getElementById("input-two").value;
@@ -111,18 +127,23 @@ const displayController = (() => {
     menuContainer.style.display = "none";
     gameContainer.style.display = "flex";
     //
-    playerOne = createPlayer(`${inputOne}`, "X");
-    playerTwo = createPlayer(`${inputTwo}`, "O");
+    playerOne = createPlayer(`${inputOne} (X's)`, "X");
+    playerTwo = createPlayer(`${inputTwo} (0's)`, "O");
     //
     playerOneName.textContent = `${playerOne.getName()}` || "Player One";
     playerTwoName.textContent = `${playerTwo.getName()}` || "Player Two";
+    //
+    playerOneName.classList.add("currentTurn");
     //
   });
   //
   restartButton.addEventListener("click", function () {
     let allBlocksCleaned = document.querySelectorAll(".block");
+    //
     menuContainer.style.display = "flex";
     gameContainer.style.display = "none";
+    //
+    playerTwoName.classList.remove("currentTurn");
     //
     gameBoard.gameBoardArray = ["", "", "", "", "", "", "", "", ""];
     //
